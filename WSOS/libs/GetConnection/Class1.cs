b1de227131cf17bb8df1bb569836a49d9bae4a10
Sourabh Jagtap <sourabhjagtap95@gmail.com>
2016-03-20 12:06:52 +0530

    CS File
    For Connection

---
+++ WSOS/libs/GetConnection/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+// Used to Get Connection To Database
+
+namespace GetConnection
+{
+    public class Class1
+    {
+        public static SqlConnection getConnected()
+        {
+            SqlConnection conn = new SqlConnection("server=SOURABH\\MYSQLSERVER;initial catalog=aspweb;integrated security=true");
+            return conn;
+        }
+    }
+}
